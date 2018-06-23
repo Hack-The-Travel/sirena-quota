44d11758a124147b3dcc367e2863c1bd5ec28a77
Sergey Popinevskiy <gurza000@gmail.com>
2018-06-23 12:02:40 +0300

    Reuqest sender should work but not work

--- checker.py
+++ checker.py
@@ -1,5 +1,23 @@
 # -*- coding: utf-8 -*-
+import requests
+import conf
+
+rq = '''<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ser="http://service.swc.comtech/">
+   <soapenv:Header/>
+   <soapenv:Body>
+      <ser:getTicketQuota>
+         <dynamicId>0</dynamicId>
+      </ser:getTicketQuota>
+   </soapenv:Body>
+</soapenv:Envelope>'''
+
+
+def get_quota():
+    r = requests.post(conf.gateway, auth=(conf.user, conf.password), data=rq)
+    r.raise_for_status()
+    print(r.content)
 
 
 if __name__ == '__main__':
     print('Debug. Trying to send request to Sirena.')
+    get_quota()
