c59296a1c143c874ea074251749507951bb58317
Sergey Popinevskiy <gurza000@gmail.com>
2018-06-23 15:29:30 +0300

    Added gateway of Sirena WS as optional parameter

--- checker.py
+++ checker.py
@@ -10,13 +10,14 @@ def extract_ticket_quota(ticket_quota_response):
     return int(matches[0])
 
 
-def get_ticket_quota(user, password):
+def get_ticket_quota(user, password, gateway='https://ws.sirena-travel.ru/swc-main/bookingService'):
     """Returns ticket quota for PoS (ППр).
 
     This method calls `getTicketQuota` method of Sirena WS.
 
     :param user: Sirena WS user with supervisor permission in the PoS.
     :param password: user password.
+    :param gateway: (optional) Sirena WS gateway to call getTicketQuota method.
     :return: ticket quota.
     :rtype: int
     """
@@ -28,7 +29,7 @@ def get_ticket_quota(user, password):
           </ser:getTicketQuota>
        </soapenv:Body>
     </soapenv:Envelope>'''
-    r = requests.post(conf.gateway, auth=(user, password), data=rq)
+    r = requests.post(gateway, auth=(user, password), data=rq)
     r.raise_for_status()
     return extract_ticket_quota(r.text)
 
