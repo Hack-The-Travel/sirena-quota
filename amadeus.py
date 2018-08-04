1265c6e28f4c673a8054b1fce904cbdaaa002efa
Sergey Popinevskiy <gurza000@gmail.com>
2018-08-04 16:24:19 +0300

    Implement checker methods
    security_authenticate - create session using Header 1.1 protocol.
    command_cryptic - call terminal command.

--- amadeus.py
+++ amadeus.py
@@ -4,9 +4,73 @@ import requests
 AWS_GATE = 'https://production.webservices.amadeus.com'
 
 
-def main():
-    pass
+def security_authenticate():
+    headers = {
+        'Content-Type': 'text/xml;charset=UTF-8',
+        'SOAPAction': 'http://webservices.amadeus.com/1ASIWASIAER/VLSSLQ_06_1_1A',
+    }
+    rq = '''<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:def="http://webservices.amadeus.com/definitions" xmlns:vls="http://xml.amadeus.com/VLSSLQ_06_1_1A">
+      <soapenv:Header>
+        <Action>http://webservices.amadeus.com/1ASIWASTAER/VLSSLQ_06_1_1A</Action>
+        <SessionId />
+      </soapenv:Header>
+      <soapenv:Body>
+        <Security_Authenticate>
+         <userIdentifier xmlns="http://xml.amadeus.com/VLSSLQ_06_1_1A">
+           <originIdentification>
+             <sourceOffice>{office}</sourceOffice>
+           </originIdentification>
+           <originatorTypeCode>U</originatorTypeCode>
+           <originator>{user}</originator>
+         </userIdentifier>
+         <dutyCode xmlns="http://xml.amadeus.com/VLSSLQ_06_1_1A">
+           <dutyCodeDetails>
+             <referenceQualifier>DUT</referenceQualifier>
+             <referenceIdentifier>SU</referenceIdentifier>
+           </dutyCodeDetails>
+         </dutyCode>
+         <systemDetails xmlns="http://xml.amadeus.com/VLSSLQ_06_1_1A">
+           <organizationDetails>
+             <organizationId>{organization}</organizationId>
+           </organizationDetails>
+         </systemDetails>
+         <passwordInfo xmlns="http://xml.amadeus.com/VLSSLQ_06_1_1A">
+           <dataLength>10</dataLength>
+           <dataType>E</dataType>
+           <binaryData>{password}</binaryData>
+         </passwordInfo>
+        </Security_Authenticate>
+        </soapenv:Body>
+    </soapenv:Envelope>'''.format(user='', password='', office='', organization='')
+    r = requests.post(AWS_GATE, headers=headers, data=rq)
+    print(r.text)
+
+
+def command_cryptic():
+    headers = {
+        'Content-Type': 'text/xml;charset=UTF-8',
+        'SOAPAction': 'http://webservices.amadeus.com/1ASIWASIAER/HSFREQ_07_3_1A',
+    }
+    rq = '''<soapenv:Envelope xmlns:def="http://webservices.amadeus.com/definitions" xmlns:hsf="http://xml.amadeus.com/HSFREQ_07_3_1A" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">
+       <soapenv:Header>
+           <def:SessionId>{session_id}|2</def:SessionId>
+       </soapenv:Header>
+       <soapenv:Body>
+           <Command_Cryptic>
+               <messageAction>
+                   <messageFunctionDetails>
+                       <messageFunction>M</messageFunction>
+                   </messageFunctionDetails>
+               </messageAction>
+               <longTextString>
+                   <textStringDetails>toqd/t-S7</textStringDetails>
+               </longTextString>
+           </Command_Cryptic>
+       </soapenv:Body>
+    </soapenv:Envelope>'''.format(session_id = '03GDRWTQT5')
+    r = requests.post(AWS_GATE, headers=headers, data=rq)
+    print(r.text)
 
 
 if __name__ == '__main__':
-    main()
+    command_cryptic()
