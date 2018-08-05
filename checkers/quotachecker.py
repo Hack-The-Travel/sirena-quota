f2d773617db2161d3e5f39390db12fc6f592023e
Sergey Popinevskiy <gurza000@gmail.com>
2018-08-05 16:51:51 +0300

    Refactoring of basic class
    1. Rename basic class.
    2. Implement response class.
    3. Add plug for `get_quota()` method.

--- checkers/quotachecker.py
+++ checkers/quotachecker.py
@@ -6,7 +6,13 @@ from jinja2 import Environment, FileSystemLoader
 here = os.path.dirname(os.path.abspath(__file__))
 
 
-class Checker(object):
+class QuotaResponse(object):
+    def __init__(self):
+        self.tickets: int = None
+        self.emds: int = None
+
+
+class QuotaChecker(object):
     def __init__(self, template_dir: str = None):
         #: String contains text of HTTP body request
         self.last_sent = None
@@ -20,6 +26,9 @@ class Checker(object):
             template_dir = os.path.join(here, template_dir)
             self.__template_env = Environment(loader=FileSystemLoader(template_dir))
 
+    def get_quota(self) -> QuotaResponse:
+        raise NotImplementedError
+
     def render_template(self, template_filename: str, context: dict = None) -> str:
         """Renders a template into a string.
 
@@ -29,8 +38,9 @@ class Checker(object):
         template = self.__template_env.get_template(template_filename)
         return template.render(context)
 
-    def request(self, url: str, method: str = 'post', auth=None, headers=None, data=None):
+    def request(self, url: str, method: str = 'post', auth=None, headers=None, data=None) -> str:
         r = requests.request(method, url, auth=auth, headers=headers, data=data)
         self.last_sent = data
         self.last_received = r.content
         r.raise_for_status()
+        return r.text  # content of the response, in unicode
