a1c3ebd0c0db7e5971d5bce174933100986442a3
Sergey Popinevskiy <gurza000@gmail.com>
2018-08-05 16:00:19 +0300

    Support templates for Sirena

--- checkers/sirena.py
+++ checkers/sirena.py
@@ -5,21 +5,12 @@ from .checker import Checker
 class SirenaChecker(Checker):
     def __init__(self,
                  user: str, password: str, endpoint: str):
-        Checker.__init__(self)
+        Checker.__init__(self, template_dir='templates')
         self.user = user
         self.password = password
         self.endpoint = endpoint
 
     def get_quota(self):
-        rq = '''<soapenv:Envelope
-           xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
-           xmlns:ser="http://service.swc.comtech/">
-           <soapenv:Header/>
-           <soapenv:Body>
-              <ser:getTicketQuota>
-                 <dynamicId>0</dynamicId>
-              </ser:getTicketQuota>
-           </soapenv:Body>
-        </soapenv:Envelope>'''
+        rq = self.render_template('sirena_get_ticket_quota.xml', context=None)
         self.request(self.endpoint, auth=(self.user, self.password), data=rq)
         return self.last_sent, self.last_received
